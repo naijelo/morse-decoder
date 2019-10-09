0056236205bc6415ef4aac6af09a85445a260f20
naijel <naijel.a@gmail.com>
2019-10-10 00:25:13 +0300

    Add: implementation

--- src/index.js
+++ src/index.js
@@ -38,9 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    
+    let stack = [];// сюда складывать готовое
+    let sub; // подстрока
+    for (let i = 0; i < (expr.length / 10); i++) {
+        let firstIndex = i * 10;
+        let lastIndex = firstIndex + 10;
+        sub = expr.substring(firstIndex, lastIndex);
+        //поделили и теперь замена
+        if (sub === "**********") {
+            stack.push(" ");
+        } else {
+            sub = stack.push(MORSE_TABLE[sub.replace(/00/g, "").replace(/10/g, ".").replace(/11/g, "-")]);
+        }
+    }
+    return stack.join("");
 }
-
+ 
 module.exports = {
     decode
 }
